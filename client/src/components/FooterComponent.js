ef037255f240f8cf32a516f73887a1216fc70293
lakshya369 <groverlakshya.25.lg@gmail.com>
2023-11-15 08:09:49 +0530

    first init

---
+++ client/src/components/FooterComponent.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+
+class Footer extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+        }
+    }
+
+render(){
+    return(
+                    <p className="footer" align="center">© Copyright Lakshya Grover</p>
+          );
+}
+
+}
+
+export default Footer;
